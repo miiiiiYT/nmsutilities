d9c23d472f10a748ca28b089fbd6c51a26e59798
Riesenpilz <66840021+Riesenpilz@users.noreply.github.com>
2021-05-24 14:35:03 +0200

    added play out packets

--- src/io/github/riesenpilz/nms/packet/playOut/PacketPlayOutOpenHorseWindowEvent.java
+++ src/io/github/riesenpilz/nms/packet/playOut/PacketPlayOutOpenHorseWindowEvent.java
@@ -2,15 +2,17 @@ package io.github.riesenpilz.nms.packet.playOut;
 
 import org.bukkit.entity.Player;
 
+import io.github.riesenpilz.nms.reflections.Field;
 import net.minecraft.server.v1_16_R3.Packet;
 import net.minecraft.server.v1_16_R3.PacketListenerPlayOut;
+import net.minecraft.server.v1_16_R3.PacketPlayOutOpenWindowHorse;
 
 /**
- * https://wiki.vg/Protocol#Open_Horse_Window<br>
- * <br>
+ * https://wiki.vg/Protocol#Open_Horse_Window
+ * <p>
  * This packet is used exclusively for opening the horse GUI. Open Window is
- * used for all other GUIs.<br>
- * <br>
+ * used for all other GUIs.
+ * <p>
  * Packet ID: 0x1E<br>
  * State: Play<br>
  * Bound To: Client
@@ -20,13 +22,39 @@ import net.minecraft.server.v1_16_R3.PacketListenerPlayOut;
  */
 public class PacketPlayOutOpenHorseWindowEvent extends PacketPlayOutEvent {
 
-	public PacketPlayOutOpenHorseWindowEvent(Player injectedPlayer) {
+	private int windowID;
+	private int slots;
+	private int entityID;
+
+	public PacketPlayOutOpenHorseWindowEvent(Player injectedPlayer, PacketPlayOutOpenWindowHorse packet) {
 		super(injectedPlayer);
+		windowID = Field.get(packet, "a", int.class);
+		slots = Field.get(packet, "b", int.class);
+		entityID = Field.get(packet, "c", int.class);
+	}
+
+	public PacketPlayOutOpenHorseWindowEvent(Player injectedPlayer, int windowID, int slots, int entityID) {
+		super(injectedPlayer);
+		this.windowID = windowID;
+		this.slots = slots;
+		this.entityID = entityID;
+	}
+
+	public int getWindowID() {
+		return windowID;
+	}
+
+	public int getSlots() {
+		return slots;
+	}
+
+	public int getEntityID() {
+		return entityID;
 	}
 
 	@Override
 	public Packet<PacketListenerPlayOut> getNMS() {
-		return null;
+		return new PacketPlayOutOpenWindowHorse(windowID, slots, entityID);
 	}
 
 	@Override
